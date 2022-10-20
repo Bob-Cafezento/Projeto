01eb6b4a7813b86d95ed056dff3cf236c93b9305
Bob-Cafezento <successstudent9@gmail.com>
2022-10-20 16:16:56 -0300

    aula 20/10/22 indo mecher com a conta

--- success-student-front/src/App.vue
+++ success-student-front/src/App.vue
@@ -3,11 +3,11 @@
 
 <template>
   <header>
-    <a href="Principal.html" id="header">
-      <img src="@/assets/imagens/logo-branca.png" title="Logo" width="100%" />
-    </a>
+      <RouterLink to="/" id="header">
+        <img src="@/assets/imagens/logo-branca.png" title="Logo" width="100" />
+      </RouterLink>
     <div class="header-item">
-      <a id="cabecalho" href="login.html">Login</a>
+      <RouterLink id="cabecalho" to="/login">Login</RouterLink>
     </div>
     <div class="header-item">
       <a id="cabecalho" href="Conta.html">Conta</a>
